b98924662c8b1e80d6c591620451b62be9c3be69
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-02-13 16:13:39 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://hn.algolia.com/api/v1/search?query=redux")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.hits);
+        setData(res.hits);
+      })
+      .catch((err) => setError(err));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="grid grid-cols-4 gap-5">
+      {data.map((item) => (
+        <div className="bg-slate-300 p-5 flex flex-col items-center" key={item.story_id}>
+          <p>{item.title}</p>
+          <p>{item.author}</p>
+          <a target="_blank" className="bg-blue-400 w-fit px-3 py-1 rounded-md text-white" href={item.url}>click go to book</a>
+          {item._tags.map((tag, index) => (
+            <div key={index}>
+              <p>{tag}</p>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
